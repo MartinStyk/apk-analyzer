14a6c998b6b46b790e239936893e4571fadd7c0f
Martin Styk <martin.styk@gmail.com>
2026-08-10 18:01:50 +0200

    Remove unused app module code

--- app/src/main/kotlin/sk/styk/martin/apkanalyzer/dependencyinjection/ApplicationModule.kt
+++ app/src/main/kotlin/sk/styk/martin/apkanalyzer/dependencyinjection/ApplicationModule.kt
@@ -1,11 +1,8 @@
 package sk.styk.martin.apkanalyzer.dependencyinjection
 
-import android.content.Context
-import com.google.firebase.analytics.FirebaseAnalytics
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
@@ -17,7 +14,4 @@ class ApplicationModule {
     @Provides
     @Singleton
     fun provideApplicationScope(): CoroutineScope = CoroutineScope(SupervisorJob())
-
-    @Provides
-    fun provideFirebaseAnalytics(@ApplicationContext context: Context): FirebaseAnalytics = FirebaseAnalytics.getInstance(context)
 }
--- app/src/main/kotlin/sk/styk/martin/apkanalyzer/util/file/GenericFileProvider.kt
+++ app/src/main/kotlin/sk/styk/martin/apkanalyzer/util/file/GenericFileProvider.kt
@@ -2,8 +2,4 @@ package sk.styk.martin.apkanalyzer.util.file
 
 import androidx.core.content.FileProvider
 
-class GenericFileProvider : FileProvider() {
-    companion object {
-        const val AUTHORITY = "sk.styk.martin.apkanalyzer"
-    }
-}
+class GenericFileProvider : FileProvider()
