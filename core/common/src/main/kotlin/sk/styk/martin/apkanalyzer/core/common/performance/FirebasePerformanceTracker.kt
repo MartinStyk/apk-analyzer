b8b6b14b3db9c7f58380b4a06912580cca2d07dc
Martin Styk <martin.styk@gmail.com>
2026-08-11 23:33:38 +0200

    Simplify performance tracker injection

--- core/common/src/main/kotlin/sk/styk/martin/apkanalyzer/core/common/performance/FirebasePerformanceTracker.kt
+++ core/common/src/main/kotlin/sk/styk/martin/apkanalyzer/core/common/performance/FirebasePerformanceTracker.kt
@@ -2,12 +2,8 @@ package sk.styk.martin.apkanalyzer.core.common.performance
 
 import com.google.firebase.perf.FirebasePerformance
 import javax.inject.Inject
-import javax.inject.Singleton
 
-@Singleton
-internal class FirebasePerformanceTracker
-@Inject
-constructor() : PerformanceTracker {
+internal class FirebasePerformanceTracker @Inject constructor() : PerformanceTracker {
     override fun startTrace(name: String): PerformanceTrace {
         val trace = FirebasePerformance.getInstance().newTrace(name)
         trace.start()
